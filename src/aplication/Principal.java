8b146e07050e7a64031a8ece4974552a2dc25822
RogersCarvalho <rogerssud@yahoo.com.br>
2022-01-21 06:15:39 -0300

    Oitavo commit - criando método Update

--- src/aplication/Principal.java
+++ src/aplication/Principal.java
@@ -18,42 +18,15 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-	     	
-		    SellerDAO sellerDaoJDBC = DaoFactory.createSellerDao();
+	     	SellerDAO sellerDaoJDBC = DaoFactory.createSellerDao();
 
-		   
+		    System.out.println("\n=== TEST 5: seller update =====");
 		    
-		    /*
-		    //System.out.println("\n=== TEST 1: findById =======");
-		    Seller seller = sellerDaoJDBC.findById(3);
-		    System.out.println(seller);
-		    */
+		    Seller s = sellerDaoJDBC.findById(1);
+		    s.setName("Martha Waine");
+		    sellerDaoJDBC.update(s);
+		    System.out.println("Update completed");
 		    
-		    /*
-		     //System.out.println("\n=== TEST 2: findByDepartment =====");
-		    Department department = new Department(2, null);
-			List<Seller> list = sellerDaoJDBC.findByDepartment(department);
-			for (Seller obj : list) {
-				System.out.println(obj);
-			}
-		    */
-		    
-		    /*
-		    //System.out.println("\n=== TEST 3: seller findAll =====");
-		    List<Seller> list = sellerDaoJDBC.findAll();
-			for (Seller obj : list) {
-				System.out.println(obj);
-			}*/
-
-		    
-		    
-		    //System.out.println("\n=== TEST 4: seller insert =====");
-		    Department department = new Department(2, null);
-		    Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		    sellerDaoJDBC.insert(newSeller);
-			System.out.println("Inserted! New id = " + newSeller.getId());
-
-
      }
 }
 
